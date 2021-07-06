91b381bc0d2dfa885adab270269388267ab2a362
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-07-06 22:11:17 +0800

    Update 全排列.py

--- 全排列.py
+++ 全排列.py
@@ -35,3 +35,37 @@ class Solution:
         res = []
         backtrack()
         return res
+
+  
+给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+
+示例:
+
+输入: n = 4, k = 2
+输出:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+
+不考虑顺序
+
+# backtrack剪枝版            
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result, track = [], []
+        self.backtrack(n, k, 1, track, result)
+        return result
+
+    def backtrack(self, n, k, start, track, result):
+        if k == 0:
+            result.append(track[:])
+            return
+        for i in range(start, n-k+2):
+            track.append(i)
+            self.backtrack(n, k-1, i+1, track, result)
+            track.pop()
